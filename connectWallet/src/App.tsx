f25af07a4bbb5b951740fd62fbfb4734b6892b96
nxchung1102 <nxchung1102@gmail.com>
2024-07-26 15:02:21 +0700

    feature sendToken & layout

---
+++ connectWallet/src/App.tsx
@@ -0,0 +1,13 @@
+import "./App.css";
+import React from "react";
+import ButtonConnect from "./components/ButtonConnect";
+
+function App() {
+  return (
+    <>
+      <ButtonConnect />
+    </>
+  );
+}
+
+export default App;
